628fb10fa8d97296113b23ca4c5d3484c5fe1c00
Drew Wells <drew.wells00@gmail.com>
2011-02-03 01:10:21 -0500

    prevented duplicate geojson.load events from screwing up things Normalized some problems when bad data comes from craigslist

--- main.js
+++ main.js
@@ -2,22 +2,23 @@ var po = org.polymaps;
 var map = po.map()
     .container(document.getElementById("map").appendChild(po.svg("svg")))
     .center({lat: 33.65, lon: -84.42})
-    .zoom(8)
-    .add(po.interact());
-var accordion;
-    
-var contents = $("#contents").ajaxStop(function(){
-    accordion = $("#contents").accordion({
-	collapsible: true,
-	active: false
-    });
+    .zoom(9)
+    .add(po.interact())
+    .add(po.image()
+	 .url(po.url("http://{S}tile.cloudmade.com"
+		     + "/1a1b06b230af4efdbb989ea99e9841af" // http://cloudmade.com/register
+		     + "/998/256/{Z}/{X}/{Y}.png")
+	      .hosts(["a.", "b.", "c.", ""])));
 
-});
-map.add(po.image()
-    .url(po.url("http://{S}tile.cloudmade.com"
-		+ "/1a1b06b230af4efdbb989ea99e9841af" // http://cloudmade.com/register
-		+ "/998/256/{Z}/{X}/{Y}.png")
-	 .hosts(["a.", "b.", "c.", ""])));
+var accordion,
+    contents = $("#contents").ajaxStop(function(){
+
+	accordion = $("#contents").accordion({
+	    collapsible: true,
+	    active: false
+	});
+
+    });
 
 $(window).resize(function(){
 
@@ -26,13 +27,12 @@ $(window).resize(function(){
 }).trigger('resize');
 map.resize();
 
-
 $.ajax('proxy.php?mode=native&url=http://atlanta.craigslist.org/nat/roo/',{
     success: function(data){
-	var doc = data;
-	var a = $("p > a", doc);
+	var doc = data,
+	    a = $("p > a", doc),
 	//$("#contents").append( a.append('<br/>') );
-	var popup = $("<div>").css({
+	    popup = $("<div>").css({
 	    width: 200,
 	    height: 200,
 	    display: 'none',
@@ -41,78 +41,77 @@ $.ajax('proxy.php?mode=native&url=http://atlanta.craigslist.org/nat/roo/',{
 	    border: 'solid 1px black',
 	    'background-color': 'white'
 	}).appendTo('body');
+
 	var links = a.get().map(function(n,i){
 	    return n.href;
 	}),
 	    l = links.length;
-	l = 25;
-
+	console.log(l)
 
 	for (var i = 0; i < l; i++) {
 
-	    parseItem(links[i], i);
+	    parseItem(links[i]);
 	}
     }
 });
+var index = 0;
+function parseItem(item){
 
-function parseItem(item, i){
     $.ajax('proxy.php?mode=native&url=' + item,{
 	success: function (data) {
-	    data = data.replace(/<link.+/gi,'');
-	    data = data.replace(/<script.+/gi,'');
-	    var title = data.match(/<title>([^<]+)/);
-	    title = title && title.length > 1 ? title[1] : '';
-	    data = data.match(/<body[\s\S\r\n]+body>/mg);
+	    var title = data.match(/<title>([^<]+)/),
+	    	title = title && title.length > 1 ? title[1] : '';
+	    data = data.replace(/(<link|<script).+/gi,'').match(/<body[\s\S\r\n]+body>/mg);
+	    
 	    if( data ){
 		data = data[0];
+	    } else {
+		return;
 	    }
-	    var images = data.match(/<img[^<]+/gi);//Preserve reference to image, but remove them to prevent unnecessary loading
-	    data = data.replace(/<img[^<]+/gi,'');
-	    
-	    var body = $("<div />").html(data);
-	    var glink = $('a[href*="maps.google.com"]',body).text(title);
-	    var userbody = $('#userbody', body).text().replace(/[\s\t][\t\s]+/gm,' ');
+
+	    var images = data.match(/<img[^<]+/gi), //Preserve reference to image, but remove them to prevent unnecessary loading
+		data = data.replace(/<img[^<]+/gi,''),
+		body = $("<div />").html(data),
+		glink = $('a[href*="maps.google.com"]',body).text(title).clone(),
+		userbody = $('#userbody', body).text().replace(/[\s\t][\t\s]+/gm,' ');
 
 	    if( glink.attr('href') ){
 		//Address is URI encoded, decode in case of problems
 		var address = glink[0].href.replace('http://maps.google.com/?q=loc%3A+','');
 
-		$.ajax('proxy.php?url=http://maps.googleapis.com/maps/api/geocode/json'
-		       + encodeURIComponent('?sensor=true&address=' + address),{
-			   success: function(data){
-			       var loc;
-			       if( data.contents.results.length ){
+		$.ajax('proxy.php?url=http://maps.googleapis.com/maps/api/geocode/json' + encodeURIComponent('?sensor=true&address=' + address),{
+		    success: function(geo){
 
-				   loc = data.contents.results[0].geometry.location;
-				   map.add(po.geoJson()
-					   .features([{
-					       geometry:{
-						   type        : "Point",
-						   coordinates : [loc.lng, loc.lat]
-					       }
-					   }])
-					   .on("load",function(){
-					       $('<h3>').append(glink).appendTo(contents);
-					       contents.append('<div>' + userbody + '</div>');
-					       
-					       
-					       $(this.container()).click(function(){
-						   //div.addClass('ui-state-highlight');
-						   //console.log(div)
-						   // popup.show()
-						   //     .css({
-						   // 	   left: 100,
-						   // 	   top: 100
-						   //     });
-						   accordion.accordion('activate',i);
-					       });
+			var loc,
+			    firstTime = true;
+			if( geo.contents.results.length ){
+			    loc = geo.contents.results[0].geometry.location;
+			    map.add(po.geoJson()
+				    .features([{
+					geometry:{
+					    type        : "Point",
+					    coordinates : [loc.lng, loc.lat]
+					}
+				    }])
+				    .on("load",function(){
+					var i = index++;
+					if (firstTime) {
+					    firstTime = false;
+					    $('<h3>').append(glink.clone()).appendTo(contents);
+					    contents.append('<div>' + userbody + '</div>');
+					    
+					    $(this.container()).click(function(){
+						//div.addClass('ui-state-highlight');
+						console.log(i, this, title)
+						accordion.accordion('activate',i);
+					    });
 
-					   }));
+					}
 
-			       }
-			       
-			   }
-		       });	
+				    }));
+			}
+		    }
+		});	
 	    }
 	}
     });
