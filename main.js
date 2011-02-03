554eda023a3470d685c77dcdc1d3bf24819a3683
Drew Wells <drew.wells00@gmail.com>
2011-02-03 00:03:07 -0500

    Fixed remaining bugs in craig scraper Ignore some stuff Pointed everything to remote sources to prevent duplication of commited code

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
 var po = org.polymaps;
 var map = po.map()
     .container(document.getElementById("map").appendChild(po.svg("svg")))
-    .center({lat: 10, lon: -30})
-    .zoom(2)
+    .center({lat: 33.65, lon: -84.42})
+    .zoom(8)
     .add(po.interact());
+var accordion;
+    
+var contents = $("#contents").ajaxStop(function(){
+    accordion = $("#contents").accordion({
+	collapsible: true,
+	active: false
+    });
 
+});
 map.add(po.image()
     .url(po.url("http://{S}tile.cloudmade.com"
 		+ "/1a1b06b230af4efdbb989ea99e9841af" // http://cloudmade.com/register
 		+ "/998/256/{Z}/{X}/{Y}.png")
 	 .hosts(["a.", "b.", "c.", ""])));
 
+$(window).resize(function(){
 
-$.ajax('proxy.php?url=http://atlanta.craigslist.org/nat/roo/',{
+    $("#map").width( this.innerWidth - contents.width() - 50).height( this.innerHeight );
+    
+}).trigger('resize');
+map.resize();
+
+
+$.ajax('proxy.php?mode=native&url=http://atlanta.craigslist.org/nat/roo/',{
     success: function(data){
-	var doc = data.contents;
+	var doc = data;
 	var a = $("p > a", doc);
 	//$("#contents").append( a.append('<br/>') );
 	var popup = $("<div>").css({
@@ -30,54 +45,75 @@ $.ajax('proxy.php?url=http://atlanta.craigslist.org/nat/roo/',{
 	    return n.href;
 	}),
 	    l = links.length;
-	l = 5;
-	while (--l) {
+	l = 25;
 
-	    $.ajax('proxy.php?url=' + links[l],{
-		type: 'GET',
-		dataType: 'json',
-	    	success: function (data) {
 
-		    var doc = data.contents;
-		    window.doc = doc;
-		    var glink = $('a[href*="maps.google.com"]',doc);
-		    var userbody = $('div', doc);
-		    console.log(glink, userbody);
+	for (var i = 0; i < l; i++) {
 
-		    if( glink[0] ){
-			//Address is URI encoded, decode in case of problems
-			var address = glink[0].href.replace('http://maps.google.com/?q=loc%3A+','');
-
-			console.log($(doc))
-			$.ajax('proxy.php?url=http://maps.googleapis.com/maps/api/geocode/json'
-			       + encodeURIComponent('?sensor=true&address=' + address),{
-			    success: function(data){
-				var loc;
-				if( data.contents.results ){
-				    loc = data.contents.results[0].geometry.location;
-				}
-				map.add(po.geoJson()
-					 .features([{
-					     geometry:{
-						 type: "Point",
-						 coordinates: [loc.lng, loc.lat]
-					     }
-					     }])
-					 .on("load",function(){
-
-					     $(this.container()).hover(function(){
-						 popup.show().html(a.eq(l)).css({
-						     left: 100,
-						     top: 100
-						 });
-					     });
-					 }));
-				
-			    }
-			});	
-		    }
-	    	}
-	    });
+	    parseItem(links[i], i);
 	}
     }
 });
+
+function parseItem(item, i){
+    $.ajax('proxy.php?mode=native&url=' + item,{
+	success: function (data) {
+	    data = data.replace(/<link.+/gi,'');
+	    data = data.replace(/<script.+/gi,'');
+	    var title = data.match(/<title>([^<]+)/);
+	    title = title && title.length > 1 ? title[1] : '';
+	    data = data.match(/<body[\s\S\r\n]+body>/mg);
+	    if( data ){
+		data = data[0];
+	    }
+	    var images = data.match(/<img[^<]+/gi);//Preserve reference to image, but remove them to prevent unnecessary loading
+	    data = data.replace(/<img[^<]+/gi,'');
+	    
+	    var body = $("<div />").html(data);
+	    var glink = $('a[href*="maps.google.com"]',body).text(title);
+	    var userbody = $('#userbody', body).text().replace(/[\s\t][\t\s]+/gm,' ');
+
+	    if( glink.attr('href') ){
+		//Address is URI encoded, decode in case of problems
+		var address = glink[0].href.replace('http://maps.google.com/?q=loc%3A+','');
+
+		$.ajax('proxy.php?url=http://maps.googleapis.com/maps/api/geocode/json'
+		       + encodeURIComponent('?sensor=true&address=' + address),{
+			   success: function(data){
+			       var loc;
+			       if( data.contents.results.length ){
+
+				   loc = data.contents.results[0].geometry.location;
+				   map.add(po.geoJson()
+					   .features([{
+					       geometry:{
+						   type        : "Point",
+						   coordinates : [loc.lng, loc.lat]
+					       }
+					   }])
+					   .on("load",function(){
+					       $('<h3>').append(glink).appendTo(contents);
+					       contents.append('<div>' + userbody + '</div>');
+					       
+					       
+					       $(this.container()).click(function(){
+						   //div.addClass('ui-state-highlight');
+						   //console.log(div)
+						   // popup.show()
+						   //     .css({
+						   // 	   left: 100,
+						   // 	   top: 100
+						   //     });
+						   accordion.accordion('activate',i);
+					       });
+
+					   }));
+
+			       }
+			       
+			   }
+		       });	
+	    }
+	}
+    });
+}
